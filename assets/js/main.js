6494d134f5544734e5d2e950adceaf6566ff1db8
Mohammed <elamrani.mohammed95@gmail.com>
2023-09-19 19:17:51 +0200

    Front TERMINER / TEST SI CHAMP VIDE / LAST COMMIT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,39 @@
-//Manipulation du DOM avec les id 
+//Manipulation du DOM
 document.addEventListener("DOMContentLoaded", function () {
-        //on enregistre les information dans des constantes
+    //Stock les données dans les constantes 
     const formulaire = document.getElementById("formulaire");
     const ville = document.getElementById("ville");
     const information = document.getElementById("information");
-//lorsque le bouton "envoyez" est cliqué ->
+
     formulaire.addEventListener("submit", function (e) {
-        e.preventDefault();
-  //on enregistre la ville rentré par l'internaute dans une variable et notre key
+// Empêche la soumission du formulaire par défaut.
+        e.preventDefault(); 
+
+        // Vérification si le champ est vide
+        if (ville.value.trim() === "") {
+            alert("Veuillez saisir une ville avant de soumettre le formulaire.");
+            return; // Ne pas continuer si le champ est vide.
+        }
+
         const city = ville.value;
         const key = "8e87a9f44d79a17621443c268a024757";
 
-          // on appel la methode Fetch qui va nous permettent de lire les données JSON 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
             .then(response => response.json())
             .then(data => {
-                // si il y'a une erreur dans les données JSON
+                // Vérification si la ville existe
                 if (data.cod === "404") {
-                     // affichage dans le DOM avec l'erreur 
                     information.innerHTML = "La ville n'est pas connue. Veuillez vérifier l'orthographe.";
                 } else {
-                // la ville existe bien ! , on recupere donc l'id de la ville rentrer 
-                const id = data.id;
-                // on recuperer l'url a redirigé 
-                const openWeatherMapURL = `https://openweathermap.org/city/${id}`;
-                  // Redirection 
-                  window.open(openWeatherMapURL, '_blank');
-                        /* window.location.href = openWeatherMapURL; */
+                    // On enregistre l'id de la ville pour pouvoir le mettre sur le lien 
+                    const id = data.id;
+                    const openWeatherMapURL = `https://openweathermap.org/city/${id}`;
+                    // Redirection dans un nouvel onglet.
+                    window.open(openWeatherMapURL, '_blank'); 
                 }
-              
             })
             .catch(error => {
-               // erreur au niveau de l'api
+                // Gestion de l'erreur lors de la récupération des données depuis l'API.
                 information.innerHTML = "Une erreur s'est produite lors de la récupération des données. Veuillez réessayer plus tard.";
             });
     });
