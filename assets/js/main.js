3c11aadd5bca0e422fdeac9a9299ec1534ec6fdb
Mohammed <elamrani.mohammed95@gmail.com>
2023-09-19 18:27:03 +0200

    gestion des erreurs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,36 @@
+//Manipulation du DOM avec les id 
 document.addEventListener("DOMContentLoaded", function () {
-    //on enregistre les information dans des constantes
+        //on enregistre les information dans des constantes
     const formulaire = document.getElementById("formulaire");
     const ville = document.getElementById("ville");
     const information = document.getElementById("information");
-    //lorsque le bouton "envoyez" est cliqué ->
+//lorsque le bouton "envoyez" est cliqué ->
     formulaire.addEventListener("submit", function (e) {
         e.preventDefault();
-    //on enregistre la ville rentré par l'internaute dans une variable
+  //on enregistre la ville rentré par l'internaute dans une variable et notre key
         const city = ville.value;
         const key = "8e87a9f44d79a17621443c268a024757";
-    // on appel la methode Fetch qui va nous permettent de lire les données JSON
+
+          // on appel la methode Fetch qui va nous permettent de lire les données JSON 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
             .then(response => response.json())
             .then(data => {
+                // si il y'a une erreur dans les données JSON
+                if (data.cod === "404") {
+                     // affichage dans le DOM avec l'erreur 
+                    information.innerHTML = "La ville n'est pas connue. Veuillez vérifier l'orthographe.";
+                } else {
+                // la ville existe bien ! , on recupere donc l'id de la ville rentrer 
                 const id = data.id;
-                console.log(id);
+                // on recuperer l'url a redirigé 
+                const openWeatherMapURL = `https://openweathermap.org/city/${id}`;
+                }
+                // Redirection 
+                window.location.href = openWeatherMapURL;
             })
             .catch(error => {
-                information.innerHTML = "La ville n'est pas connu. Veuillez vérifier l'orthographe.";
+               
+                information.innerHTML = "Une erreur s'est produite lors de la récupération des données. Veuillez réessayer plus tard.";
             });
     });
 });
